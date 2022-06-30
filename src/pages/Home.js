06aa271fbfec0c1f7ba8ca05a8d227572a014109
rabbigolam <rabbi.golem@decenternet.net>
2022-06-30 16:32:17 +0600

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
+    const router = useNavigate();
     return (
         <div>
             Home
-            <a href="osiris://odin"> click here </a>
+            <a href="osiris://odin" > click here </a>
+            <button onClick={() => router('osiris://odin')}> click on button </button>
         </div>
     );
 }
